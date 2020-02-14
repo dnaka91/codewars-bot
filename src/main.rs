fdf13f9ecd9292f277b958e4506752b498fa5b3c
Dominik Nakamura <dnaka91@gmail.com>
2020-02-14 22:16:21 +0900

    Use write! instead of format!

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![allow(dead_code)]
 
 use std::env;
+use std::fmt::Write;
 
 use anyhow::{Context, Result};
 use futures::prelude::*;
@@ -99,7 +100,13 @@ async fn run() -> Result<()> {
     while let Some(event) = r.next().await {
         info!("EVENT {:?}", event);
 
-        if let slack::Event::Message { channel, text, user,.. } = event {
+        if let slack::Event::Message {
+            channel,
+            text,
+            user,
+            ..
+        } = event
+        {
             if channel == target_channel && text.starts_with(&prefix) {
                 let response = match commands::parse(&text[prefix.len()..]) {
                     Ok(cmd) => match cmd {
@@ -119,35 +126,32 @@ async fn run() -> Result<()> {
                                 challenges.sort_by(|a, b| a.completed_at.cmp(&b.completed_at));
                                 challenges.reverse();
 
-                                response.push_str(&format!(
+                                write!(
+                                    &mut response,
                                     "\n\n`{}` - {} total challenges",
                                     user, challenge_resp.total_items
-                                ));
+                                )?;
 
-                                challenges
-                                    .into_iter()
-                                    .take(3)
-                                    .filter_map(|c| {
-                                        if let Some(name) = c.name.as_ref() {
-                                            Some(format!(
-                                                "\n*{}* solved at _{}_ in *{}*",
-                                                name,
-                                                c.completed_at.format("%Y/%m/%d"),
-                                                c.completed_languages
-                                                    .into_iter()
-                                                    .collect::<Vec<_>>()
-                                                    .join(", ")
-                                            ))
-                                        } else {
-                                            None
-                                        }
-                                    })
-                                    .for_each(|s| response.push_str(&s));
+                                for challenge in challenges.into_iter().take(3) {
+                                    if let Some(name) = challenge.name {
+                                        write!(
+                                            &mut response,
+                                            "\n*{}* solved at _{}_ in *{}*",
+                                            name,
+                                            challenge.completed_at.format("%Y/%m/%d"),
+                                            challenge
+                                                .completed_languages
+                                                .into_iter()
+                                                .collect::<Vec<_>>()
+                                                .join(", ")
+                                        )?;
+                                    }
+                                }
                             }
 
                             response
                         }
-                        Command::Help => format!("Hey there {}!", user)
+                        Command::Help => format!("Hey there <@{}>! This is still in TODO!", user),
                     },
                     Err(e) => format!("Unknown command: {}", e),
                 };
